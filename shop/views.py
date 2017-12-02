f30770f3cee60e6713205270b93ac0dae83629b9
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2017-12-02 19:21:24 +0200

    регистрация на основе base заработала

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from .models import Category, Product
 from django.views import View
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from cart.forms import CartAddProductForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Регистрация успешна')
+                else:
+                    return HttpResponse('Пользователь отсутствует')
+            else:
+                return HttpResponse('Неверный login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
 
 
 def product_list(request, category_slug=None):
@@ -41,28 +63,17 @@ def product_detail(request, id, slug):
                    'cart_product_form': cart_product_form})
 
 
-# class EIndexView(View):
-#     def get(self, request):
-#
-#         # Забираем все продукты отсортировав их по дате обновл-я
-#         all_products = Product.objects.filter(available=True).order_by('-updated')
-#         # Создаём Paginator, в который передаём продукты и указываем,
-#         # что их будет 3 штук на одну страницу
-#         current_page = Paginator(all_products, 3)
-#
-#         # Pagination в django_bootstrap3 посылает запрос вот в таком виде:
-#         # "GET /?page=2 HTTP/1.0" 200,
-#         # Поэтому нужно забрать page и попытаться передать его в Paginator,
-#         # для нахождения страницы
-#         page = request.GET.get('page')
-#         try:
-#             # Если существует, то выбираем эту страницу
-#             context['article_lists'] = current_page.page(page)
-#         except PageNotAnInteger:
-#             # Если None, то выбираем первую страницу
-#             context['article_lists'] = current_page.page(1)
-#         except EmptyPage:
-#             # Если вышли за последнюю страницу, то возвращаем последнюю
-#             context['article_lists'] = current_page.page(current_page.num_pages)
-#
-#         return render_to_response('home/index.html', context)
+# def user_login(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = auth.authenticate(username=username, password=password)
+#     if user is not None and user.is_active:
+#         # Правильный пароль и пользователь "активен"
+#         auth.login(request, user)
+#         # Перенаправление на "правильную" страницу
+#         return HttpResponseRedirect("/account/loggedin/")
+#     else:
+#         # Отображение страницы с ошибкой
+#         return HttpResponseRedirect("/account/invalid/")
+
+
