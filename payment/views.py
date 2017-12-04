0115c9ba38f37e893fe69f6b0840bea65e9105ca
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2017-12-04 21:37:42 +0200

    Update views.py

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from paypal.standard.forms import PayPalPaymentsForm
 from orders.models import Order
